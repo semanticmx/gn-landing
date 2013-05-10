b1c3ce0f2b8cff696fbc904a8bfa7ec6f63747db
gjpv <gjpv@linux-5jy8.site>
2013-05-10 00:46:13 -0500

    GP, first commit Setup Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GnLanding::Application.config.secret_token = '35b6317adf595b25b40b81e9e868033779f235f427448aa34b6057d1a6941c6e4790b7994ba20149d4b3ee2017bd1626a0b1293b68adb8911cf7916567457712'
